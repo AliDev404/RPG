63185672001d244b0ed7f721e608df573cd5a65d
Ali <mohammad.ali.dev.64@gmail.com>
2025-08-26 22:06:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import Character as c
 import Battle as b
+import save
+
 
 def Start():
-    name=input("Enter your name: ")
-    return name
+    while True:
+        new=input("1) New game: \n2) Load Game\n3) Show all saves\n").strip()
+        if new=='1' or new == '2':
+            name=input("Enter your name: ").strip().lower()
+            if new=='1':
+                Save(name,0)
+                break
+            elif new=='2':
+                load(name)
+                break
+        elif new=='3':
+            All_saves()
 
+        else:
+            pass
+        
 
 
 
@@ -15,12 +30,28 @@ The King has promised a reward to anyone who can defeat the Shadow Beast that dw
     choose=input("***\n1)Buy a healing potion from the shop.\n2)Speak to the old wise man.\n3)Head straight into the forest.\n***\n")
     
 
-player1 = c.Player(Start())
-print(player1)
 enemy1= c.Enemy("Goblin", hp=15, atk=2, defense=1, reward_gold=5, reward_items=["Rusty Dagger"])
 
-def fight():
+
+
+def Fight():
     battle = b.battle(player1,enemy1)
     battle.start()
 
-fight()
+def Load(name):
+    file = save.SaveLoader()
+    return file.load(name)
+
+def All_saves():
+    file = save.SaveLoader()
+    return file.show_all_save()
+
+def Save(name,current_level):
+    file = save.SaveLoader()
+    return file.save(name,current_level)
+
+name = Start()
+player = c.Player(name)
+print(player)
+
+load(name)
